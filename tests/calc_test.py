7a33ab34cabd2d97d94917cd4110ef3828d3e050
ponomkir42 <110480842+ponomkir42@users.noreply.github.com>
2022-09-06 22:33:03 +0500

    Create calc_test.py

---
+++ tests/calc_test.py
@@ -0,0 +1,19 @@
+from app.calculator import Calculator
+import pytest
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_adding_calculate_correctly(self):
+        assert self.calc.adding(self, 2, 5) == 7
+
+    def test_subtraction_calculate_correctly(self):
+        assert self.calc.subtraction(self, 10, 5) == 5
+
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(self, 10, 5) == 2
